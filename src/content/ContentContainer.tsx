11653e4727fd42cfcaa2de8abaf3cab94e30e8b1
Xiaorui Wang <xiaorui.wang@usi.ch>
2025-06-03 21:22:55 +0200

    complete content cell layout

--- src/content/ContentContainer.tsx
+++ src/content/ContentContainer.tsx
@@ -3,7 +3,7 @@
  * @Email: xiaorui.wang@usi.ch
  * @Date: 2025-06-02 22:34:33
  * @LastEditors: Xiaorui Wang
- * @LastEditTime: 2025-06-03 17:12:09
+ * @LastEditTime: 2025-06-03 21:19:36
  * @Description:
  * 
  * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
@@ -12,6 +12,11 @@
 import user from "../assets/user.svg"
 import more_account from "../assets/more_account.svg"
 import contentImg from "./imgs/contentImage1.jpeg"
+import comments from "../assets/comments.svg"
+import repost from "../assets/repost.svg"
+import like from "../assets/like.svg"
+import views from "../assets/views.svg"
+import share from "../assets/share.svg"
 
 interface PostItem {
     userImgUrl: string,
@@ -93,7 +98,7 @@ export const ContentContainer = () => {
     return (
         <div className="py-2.5">
             {
-                sources.map((element, index, array) => 
+                sources.map((element, index, array) =>
                     <PostCell key={index} {...element} />
                 )
             }
@@ -103,7 +108,7 @@ export const ContentContainer = () => {
 
 const PostCell = (item: PostItem) => {
     return (
-        <div className="flex items-start gap-2.5 p-2.5 border-b-[0.5px] border-gray-300 hover:bg-gray-100">
+        <div className="flex items-start gap-2.5 px-2.5 py-0.5 border-b-[0.5px] border-gray-300 hover:bg-gray-100">
             <img src={user} alt="userimg" />
             <div className="flex flex-col">
                 <TopBar userName={item.userName} userAccount={item.userAccount} time={item.time} />
@@ -111,13 +116,13 @@ const PostCell = (item: PostItem) => {
                     {item.content}
                 </div>
                 <img src={contentImg} alt="contentimage" />
-
+                <BottomBar commentCount={item.commentCount} repostCount={item.repostCount} likeCount={item.likeCount} viewCount={item.viewCount} />
             </div>
         </div>
     );
 }
 
-const TopBar = ({userName, userAccount, time}: {userName: string, userAccount: string, time: string}) => {
+const TopBar = ({ userName, userAccount, time }: { userName: string, userAccount: string, time: string }) => {
 
     const getMonthFromNumber = (num: number) => {
         switch (num) {
@@ -132,26 +137,63 @@ const TopBar = ({userName, userAccount, time}: {userName: string, userAccount: s
             case 8: return "Sep";
             case 9: return "Oct";
             case 10: return "Nov";
-            case 11: return "Dec";        
+            case 11: return "Dec";
             default: return "";
         }
     };
 
 
     let date = new Date(time);
-    let timestr = `${ getMonthFromNumber(date.getMonth())}  ${date.getDate()}`
+    let timestr = `${getMonthFromNumber(date.getMonth())}  ${date.getDate()}`
 
 
     return (
         <div className="flex justify-between items-center">
             <div className="flex items-center">
                 <div className="font-bold">{userName}</div>
-                <div className="text-gray-700 text-sm">{"@"+userAccount}</div>
+                <div className="text-gray-700 text-sm">{"@" + userAccount}</div>
                 <div className="mx-0.5">・</div>
-                <div>{timestr }</div>
+                <div>{timestr}</div>
             </div>
             <div>
                 <img src={more_account} alt="more" />
+            </div>
+        </div>
+    );
+}
+
+const BottomBar = ({
+    commentCount,
+    repostCount,
+    likeCount,
+    viewCount }: {
+        commentCount: string,
+        repostCount: string,
+        likeCount: string,
+        viewCount: string
+    }) => {
+    return (
+        <div className="flex justify-between items-center pt-2">
+            <div className="flex justify-around items-center text-sm gap-10">
+                <div className="flex justify-center items-center px-1 py-0.5 hover:bg-blue-200 hover:rounded-xl">
+                    <img className="w-[20px]" src={comments} alt="comments" />
+                    <div>{commentCount}</div>
+                </div>
+                <div className="flex justify-center items-center  px-1 py-0.5 hover:bg-green-200 hover:rounded-xl">
+                    <img className="w-[20px]" src={repost} alt="repost" />
+                    <div>{repostCount}</div>
+                </div>
+                <div className="flex justify-center items-center  px-1 py-0.5 hover:bg-red-200 hover:rounded-xl">
+                    <img className="w-[20px]" src={like} alt="like" />
+                    <div>{likeCount}</div>
+                </div>
+                <div className="flex justify-center items-center  px-1 py-0.5 hover:bg-blue-200 hover:rounded-xl">
+                    <img className="w-[20px]" src={views} alt="views" />
+                    <div>{viewCount}</div>
+                </div>
+            </div>
+            <div className="flex justify-center items-center px-1 py-1 hover:bg-blue-200 hover:rounded-xl">
+                <img src={share} alt="share" />
             </div>
         </div>
     );
